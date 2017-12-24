383d96c495ed7861a1c90ac92b45bbe8d2cebd8c
dubeyvivek48 <dubeyvivek48@gmail.com>
2017-12-25 03:27:01 +0530

    mobi detail added

--- js/main.js
+++ js/main.js
@@ -6,15 +6,14 @@ $(document).ready(
         $('#searchForm').on('submit',(e)=>{
             searchText= $('#searchMobi').val();
             getMovies(searchText);
-            console.log(searchText);
-            
+                      
             e.preventDefault();
         });
     });
     function getMovies(searchText){
         axios.get('http://www.omdbapi.com/?s='+searchText +'&apikey=34d9da6c')
         .then( (Response)=>{
-            console.log(Response);
+           // console.log(Response);
             let movies=Response.data.Search;
             let output='';
             $.each(movies,(index,movie)=>{
@@ -47,10 +46,41 @@ $(document).ready(
         let movieId=sessionStorage.getItem('movieId');
 
 
-        axios.get('http://www.omdbapi.com/?i='+movieId )
-        .then( (Response)=>{
-            console.log(Response);
-                        
+        axios.get('http://www.omdbapi.com/?i='+movieId+'&apikey=34d9da6c')
+        .then( (response)=>{
+            console.log(response);
+            let movie=response.data;
+            let output=`
+            <div class="row">
+            <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+                <h2>${movie.Title}</h2>
+                <ul class="list-group">
+                <li class="list-group-item"><strong>   Genre: </strong>${movie.Genre}</li>
+                <li class="list-group-item"><strong>   Released: </strong>${movie.Released}</li>
+                <li class="list-group-item"><strong>Rated:</strong>${movie.Rated}</li>
+                <li class="list-group-item"><strong>  imdbRating:       </strong>${movie.imdbRating }</li>
+                <li class="list-group-item"><strong>  Director  :</strong>${movie.Director}</li>
+                <li class="list-group-item"><strong> Writer:   </strong>${movie.Writer}</li>
+                <li class="list-group-item"><strong>  Actor:  </strong>${movie.Actor}</li>
+                
+                
+                </ul>
+            </div>
+            </div>
+            <div class="row">
+            <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            </div>
+            </div>
+
+
+
+            `
+              $('#mobiInfo').html(output);       
         })
         .catch((err)=>{
             console.log(err);
