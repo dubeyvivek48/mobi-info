defed41daedb8a9953f705e7efb67d0e1fd5188b
dubeyvivek48 <dubeyvivek48@gmail.com>
2017-12-25 03:34:54 +0530

    mobi detail added and api work

--- js/main.js
+++ js/main.js
@@ -74,6 +74,10 @@ $(document).ready(
             <div class="well">
             <h3>Plot</h3>
             ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html"  class="btn btn-default">Go back to search</a>
+
             </div>
             </div>
 
